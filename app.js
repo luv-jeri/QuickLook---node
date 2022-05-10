2d8516809fffaefeb6d2ea1d8e03e3392d6b9b66
luv-jeri <luvvjeri@gmail.com>
2022-05-10 18:42:33 +0530

    -

--- app.js
+++ app.js
@@ -1,15 +1,52 @@
 const chalk = require('chalk');
+const http = require('http');
 
-const __ = console.log;
-
-// __(global); // ` global object same for all the files in the project
-// __(__dirname); // ` Current directory
-// __(__filename); // ` current file name and path
-//__(process); // ` cpu usage
-
-//# Attach a global property to the global object
-global.sayHello = () => {
-  __(chalk.red('Hello World'));
+global.__ = console.log;
+global._ = (_) => {
+  __(chalk.blue.bgBlack(_));
 };
 
-sayHello();
+const server = http.createServer((req, res) => {
+  //  _(req); // ` Details of the request
+  // _(req.url); // ` The URL of the request
+  // __(res); // ` Details of the response
+  //# response = {}
+
+  res.writeHead(200, { 'Content-Type': 'application/json', isOK: true });
+  //` response = {
+  //`  header : {
+  //`     'Content-Type': 'application/json'
+  //`      status: 200
+  //`  }
+  //` }
+
+  res.write(
+    JSON.stringify({
+      app: 'Node.js',
+      version: '1.0.0',
+      author: 'Rajesh',
+      url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+    })
+  );
+
+  // ` response = {
+  // `    header: {
+  // `    'Content-Type': 'application/json',
+  // `    status: 200,
+  // `  },
+  // `  body: {
+  // `    app: 'Node.js',
+  // `    version: '1.0.0',
+  // `    version: '1.0.0',
+  // `    author: 'Rajesh',
+  // `    url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+  // `  },
+  // ` }
+
+  // __(res); // ` Details of the response
+  res.end();
+});
+
+server.listen('8080', () => {
+  _('Server is running on port 8080');
+});
