de8eba5a1cccfff29adbaf9da7a23ed7f34bdf3e
luv-jeri <luvvjeri@gmail.com>
2022-05-10 18:22:56 +0530

    -

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const chalk = require('chalk');
+
 const __ = console.log;
 
+// __(global); // ` global object same for all the files in the project
+// __(__dirname); // ` Current directory
+// __(__filename); // ` current file name and path
+//__(process); // ` cpu usage
 
+//# Attach a global property to the global object
+global.sayHello = () => {
+  __(chalk.red('Hello World'));
+};
+
+sayHello();
