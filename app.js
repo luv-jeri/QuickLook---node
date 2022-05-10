b904f6ed8ef4c7de0d9ffdcbe97c811fe9895bf4
luv-jeri <luvvjeri@gmail.com>
2022-05-10 18:15:17 +0530

    -

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-const chalk = require('chalk');
-const __ = console.log;
+const exported_object_of_that_file = require('./logs');
 
+const { _e, _s, _ } = exported_object_of_that_file;
 
+console.log(module); //` (global) module object with all the properties and methods of the module
+
+_e('I am an error');
+_s('I am a success');
+_('I am a message');
---
+++ logs.js
@@ -0,0 +1,20 @@
+const chalk = require('chalk');
+const __ = console.log;
+
+const _e = (to_show) => {
+  __(chalk.red.bgBlack(to_show));
+};
+
+const _s = (to_show) => {
+  __(chalk.green.bgWhite(to_show));
+};
+
+const _ = (to_show) => {
+  __(chalk.white.bgBlack(to_show));
+};
+
+module.exports = {
+  _e,
+  _s,
+  _,
+};
